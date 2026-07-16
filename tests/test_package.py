3aa547c14d405c605e64b8a566460a0077d24903
sam <samantharamsey@live.com>
2026-07-16 11:35:36 -0400

    added package import test

---
+++ tests/test_package.py
@@ -0,0 +1,8 @@
+"""Basic tests for the tno_dynamics package."""
+
+
+def test_package_import() -> None:
+    """Verify that the top-level package can be imported."""
+    import tno_dynamics
+
+    assert tno_dynamics is not None
